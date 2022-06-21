0a44ae51680456ce8384eb816e2d38be820ce08d
Russell Ventura <russellventura2013@gmail.com>
2022-06-21 13:44:01 +0100

    Logo Added

--- my-app/src/Components/Header/index.js
+++ my-app/src/Components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../Assets/PhoenixLogo.png"; 
+import logo from "../../Assets/PhoenixLogo.png"; 
 
 //Header contains phoenix logo - img 
 //Title 
@@ -8,9 +8,11 @@ import "../../Assets/PhoenixLogo.png";
 export default function Header(){
     return(
         <div className="header-container">
-        <img className="logo" src="../../Assets/PhoenixLogo.png" alt="Logo"></img>
-        <h1 className="header">School Of Code - Help the camper</h1>
-        <a className="help-link"href="https://www.w3schools.com">Help?</a>
+            <img className="logo" src={logo} alt="Logo"></img>
+            <h1 className="header">School Of Code - Help the camper</h1>
+            <a className="help-link"href="https://www.w3schools.com">Help?</a>
         </div>
     )
 }
+
+//<img src = {this.props.url} alt='something'/> 
--- my-app/src/Components/Nav/index.js
+++ my-app/src/Components/Nav/index.js
@@ -12,14 +12,14 @@ export default function Nav(){
             <label className="category-select" >Week Number: </label>
 
           <select className="select-week" name="week" id="week">
-            <option value="week-1">Week 1</option>
-            <option value="week-2">Week 2</option>
-            <option value="week-3">Week 3</option>
-            <option value="week-4">Week 4</option>
-            <option value="week-5">Week 5</option>
-            <option value="week-6">Week 6</option>
-            <option value="week-7">Week 7</option>
-            <option value="week-8">Week 8</option>
+            <option value="week1">Week 1</option>
+            <option value="week2">Week 2</option>
+            <option value="week3">Week 3</option>
+            <option value="week4">Week 4</option>
+            <option value="week5">Week 5</option>
+            <option value="week6">Week 6</option>
+            <option value="week7">Week 7</option>
+            <option value="week8">Week 8</option>
 
 
           </select>
