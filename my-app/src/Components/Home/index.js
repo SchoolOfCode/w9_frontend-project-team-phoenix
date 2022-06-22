5b7d1890f6aa93eafb7f2866c06ef96873161ffe
Abdullahi <amohamoud1999@gmail.com>
2022-06-22 15:31:40 +0100

    home page

--- my-app/src/Components/Home/index.js
+++ my-app/src/Components/Home/index.js
@@ -2,6 +2,8 @@
 //Our text
 import Footer from "../Footer"
 import Header from "../Header"
+import "./index.css"
+import pic from "../../Assets/workshops.jpg"
 
 //Why we Created
 //How to use
@@ -10,13 +12,20 @@ export default function Home(){
     return(
         <>
         <Header />
-        <h1>Welcome to Team Phoenix's Bootcamper Assistant tool!</h1>
-        <h2>We created this service to help our fellow bootcampers and ourselves by providing important information each week</h2>
-        <h3>To access our resources, please click the button below! Enjoy :)</h3>
-        <a href="http://localhost:3002/Campers">
-            <button>Click here for access</button>
-        </a>
-        <Footer />
+        <div className="Home-container">
+            <div className="text-container">
+            <img className="home-image" src={pic} alt="Chris"></img>
+                <div class="centered">
+                <h1 className="home-text">Welcome to Team Phoenix's Bootcamper Assistant tool!</h1>
+                <h2 className="home-text">We created this service to help our fellow bootcampers and ourselves by providing important information from each week</h2>
+                <h3 className="home-text">To access our resources, please click the button below! Enjoy :)</h3>
+                <a href="http://localhost:3002/Campers">
+                    <button className="home-button">Click here for access</button>
+                </a>
+            </div>
+                </div>
+        </div>
+        <Footer /> 
         </>
     )
 }
