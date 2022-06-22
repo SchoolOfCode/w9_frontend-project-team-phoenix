ab8121bcaff6ef667830cc16045cf4d0a3c4dc04
Russell Ventura <russellventura2013@gmail.com>
2022-06-22 09:34:54 +0100

    Minor updates to front end

--- my-app/src/Components/Nav/index.js
+++ my-app/src/Components/Nav/index.js
@@ -9,16 +9,16 @@ export default function Nav({handleSubmit, onchange}){
           <div className="week-container">
             <div className="category-form">
             <form onSubmit={handleSubmit}>
-              <label className="category-select" >Please select the Week: </label>
+              <label className="category-select" ></label>
                 <select className="select-week" name="week" id="week" onChange={onchange}>
-                  <option value="week1">Week 1 - Intro to SoC</option>
-                  <option value="week2">Week 2 - Fundamentals of JavaScript</option>
-                  <option value="week3">Week 3 - Creating Apps with User in mind</option>
-                  <option value="week4">Week 4 - Server side programming</option>
+                  <option value="week1">Week 1 - Intro to the School of Code Way</option>
+                  <option value="week2">Week 2 - Fundamentals of proghramming with JavaScript</option>
+                  <option value="week3">Week 3 - Creating applications with User in mind</option>
+                  <option value="week4">Week 4 - The world of server side programming</option>
                   <option value="week5">Week 5 - Database & Conventions</option>
                   <option value="week6">Week 6 - Robust Code in Systems</option>
-                  <option value="week7">Week 7 - Component based thinking & Frameworks</option>
-                  <option value="week8">Week 8 - Developing user-centric apps in teams</option>
+                  <option value="week7">Week 7 - Component-based thinking & Frameworks</option>
+                  <option value="week8">Week 8 - Developing good, user-centric products in teams</option>
                 </select>
                 <input className="submit-button" type="submit" value="Submit"></input>
               </form>
