1dd68645c64307d677dd3916d44f3bd840f12e4e
Abdullahi <amohamoud1999@gmail.com>
2022-06-22 21:50:54 +0100

    Feedback partially done

--- my-app/src/Components/Feedback/index.js
+++ my-app/src/Components/Feedback/index.js
@@ -1,10 +1,47 @@
 import Header from "../Header";
+import { useState, useEffect } from "react";
+import "./index.css"
 
 export default function Feedback(){
+
+    const [form, setForm] = useState(
+        <div className="feedback-container">
+        <form className="form">
+            <div className="name-container">
+            <p className="bootcamper-name">Bootcamper Name:</p>
+                <input className="name"
+                    type="text"
+                    placeholder="first name"
+                />
+                <input className="name"
+                    type="text"
+                    placeholder="last name"
+                />
+            </div>
+            <br></br>
+            <br></br>
+            <p className="feedback">Feedback:</p>
+            <textarea className="feedback-textbox"
+                placeholder="Comment"
+                name="comment"
+            />
+            <br></br>
+            <br></br>
+        </form>
+        </div>
+    )
+
+    function onSubmit(){
+       return (
+           setForm(<h1>Thank you for your feedback, we will get back to your shortly!</h1>)
+       )
+    }
+
     return(
-        <>
+    <>
         <Header />
-        <h1>Feedback form</h1>
-        </>
+        {form}
+        <button onClick={onSubmit} className="button">Submit</button>
+    </>
     )
 }
