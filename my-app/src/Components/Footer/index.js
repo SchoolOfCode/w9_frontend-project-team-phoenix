bfc85452358471f07f8da922b2a1fea53e85462d
Abdullahi <amohamoud1999@gmail.com>
2022-06-23 12:03:31 +0100

    icons

--- my-app/src/Components/Footer/index.js
+++ my-app/src/Components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {SocialMediaIconsReact} from 'social-media-icons-react';
+
 
 export default function Footer(){
     return(
@@ -6,9 +8,9 @@ export default function Footer(){
         <div className="footer-container">
             <div className="footer">
                 <div className="social">
-                    <div className="facebook"><a href="https://www.facebook.com/schoolofcode/">Facebook</a></div>
-                    <div className="twitter"><a href="https://twitter.com/theSchoolOfCode">Twitter</a></div>
-                    <div className="linked-in"><a href="https://www.linkedin.com/school/school-of-code/">LinkedIn</a></div>
+                    <SocialMediaIconsReact borderColor="transparent" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://twitter.com/theSchoolOfCode" size="38" />
+                    <SocialMediaIconsReact borderColor="transparent" icon="facebook" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://www.facebook.com/schoolofcode/" size="38" />
+                    <SocialMediaIconsReact borderColor="transpraent" icon="linkedin" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://www.linkedin.com/school/school-of-code/" size="38" />
                 </div>
             <p className="copyright">© Copyright Phoenix Group 2022 | All rights reserved | In Association with School of Code</p>
             <p className="feedback-link"><a href="http://localhost:3002/feedback">feedback</a></p>
