3f3c974007ff45e45862198ab7fa37d801c36f37
Abdullahi <amohamoud1999@gmail.com>
2022-06-23 12:11:54 +0100

    icons update

--- my-app/src/Components/Footer/index.js
+++ my-app/src/Components/Footer/index.js
@@ -8,8 +8,8 @@ export default function Footer(){
         <div className="footer-container">
             <div className="footer">
                 <div className="social">
-                    <SocialMediaIconsReact borderColor="transparent" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://twitter.com/theSchoolOfCode" size="38" />
-                    <SocialMediaIconsReact borderColor="transparent" icon="facebook" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://www.facebook.com/schoolofcode/" size="38" />
+                    <SocialMediaIconsReact borderColor="#F87F25" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://twitter.com/theSchoolOfCode" size="38" />
+                    <SocialMediaIconsReact borderColor="#F87F25" icon="facebook" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://www.facebook.com/schoolofcode/" size="38" />
                     <SocialMediaIconsReact borderColor="transpraent" icon="linkedin" iconColor="rgba(255,255,255,1)" backgroundColor="transparent" url="https://www.linkedin.com/school/school-of-code/" size="38" />
                 </div>
             <p className="copyright">© Copyright Phoenix Group 2022 | All rights reserved | In Association with School of Code</p>
