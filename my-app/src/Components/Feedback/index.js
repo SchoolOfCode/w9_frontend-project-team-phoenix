a47249877458b0c68fa2e5a30b18cd87d43878b2
Russell Ventura <russellventura2013@gmail.com>
2022-06-29 13:56:11 +0100

    Feedback Page ClassNames refactor + CSS

--- my-app/src/Components/Feedback/index.js
+++ my-app/src/Components/Feedback/index.js
@@ -7,10 +7,10 @@ export default function Feedback(){
     const [form, setForm] = useState(
         //Sets original stae to the form
         <div className="feedback-container">
-        <form className="form">
-            <h1 className="h1-feedback">Bootcamp Buddy - Feedback Form</h1>
-            <div className="name-container">
-            <p className="bootcamper-name">Full Name:</p>
+        <form className="feedback-form">
+            <h1 className="feedback-title">Bootcamp Buddy - Feedback Form</h1>
+            <div className="fullname-container">
+            <p className="name-label">Full Name:</p>
                 <input className="name"
                     type="text"
                     placeholder="first name"
@@ -22,7 +22,7 @@ export default function Feedback(){
             </div>
             <br></br>
             <br></br>
-            <p className="feedback">Please leave your feedback below:</p>
+            <p className="feedback-prompt">Please leave your feedback below:</p>
             <textarea className="feedback-textbox"
                 placeholder="Comment"
                 name="comment"
@@ -38,7 +38,7 @@ export default function Feedback(){
            setForm(
             //re-renders page on submit to have following message
             <div className="refresh-container">
-           <h1>Thank you for your feedback, we will get back to your shortly!</h1>
+           <h1 className="feedback-reply">Thank you for your feedback, we will get back to your shortly!</h1>
            </div>
            )
        )
